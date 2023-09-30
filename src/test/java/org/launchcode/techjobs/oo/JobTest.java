60eb7fc6c5a852feee59fe1a47f78dcaf586fc14
keydamm <keyonamdamm@hotmail.com>
2023-09-29 22:19:47 -0500

    Task 4 complete

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,40 @@
 package org.launchcode.techjobs.oo;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class JobTest {
     //TODO: Create your unit tests here
+    @Test
+    public void testSettingJobId() {
+        Job jobOne = new Job();
+        Job jobTwo = new Job();
+        assertNotEquals(jobOne, jobTwo);
+    }
+
+    @Test
+    public void testJobConstructorSetsALlFields() {
+        Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertTrue(job instanceof Job);
+        assertTrue(job.getEmployer() instanceof Employer );
+        assertTrue(job.getLocation() instanceof Location );
+        assertTrue(job.getPositionType() instanceof PositionType );
+        assertTrue(job.getCoreCompetency() instanceof CoreCompetency );
+
+        assertEquals("Product tester", job.getName());
+        assertEquals("ACME", job.getEmployer().getValue());
+        assertEquals("Desert", job.getLocation().getValue());
+        assertEquals("Quality control", job.getPositionType().getValue());
+        assertEquals("Persistence", job.getCoreCompetency().getValue());
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job jobOne = new Job();
+        jobOne.setName("Product tester");
+        Job jobTwo = new Job();
+        jobTwo.setName("Product tester");
+        assertEquals(jobOne.getName(), jobTwo.getName());
+        assertFalse(jobOne.getId() == jobTwo.getId());
+    }
 }
